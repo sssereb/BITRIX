8ddf167fd9766d8e30791fcf23803efe40b08e51
sssereb <31897861+sssereb@users.noreply.github.com>
2020-01-25 21:26:00 +0200

    Add files via upload

---
+++ close_kp.php
@@ -0,0 +1,69 @@
+﻿<?php
+
+// 
+
+
+
+$deal_id = $_REQUEST['FIELDS']['ID'];
+
+
+
+$deal_data = executeREST('crm.deal.get', array('ID' => intval($_REQUEST['data']['FIELDS'][$ID_IN])));
+
+
+
+$main_kp = $deal_data['result']['QUOTE_ID'];
+
+$main_lead = $deal_data['result']['LEAD_ID'];
+
+$kp_id = $_REQUEST2['FIELDS']['ID'];
+
+$kp_data = executeREST('BX24.callMethod(	"crm.quote.list", 	{ 		order: { "STATUS_ID": "ASC" },		
+filter: { "=LEAD_ID": $main_lead },		select: [ "ID",  "STATUS_ID" ]	}, 	function(result){})', 
+array('ID' => intval($_REQUEST2['data']['FIELDS']['ID'])));
+
+
+
+foreach ($kp_data as $kp_id)
+{
+if ($kp_data['result']['ID'] == $main_kp) 
+executeREST ('crm.quote.update', 
+array($crm[result],
+array('ID' => $kp_data['result']['ID'], 
+      'FIELDS' => '{ "STATUS_ID": "APPROVED" }')));
+if ($kp_data['result']['ID'] == $main_kp) 
+executeREST ('crm.quote.update', 
+array($crm[result],
+array('ID' => $kp_data['result']['ID'], 
+      'FIELDS' => '{ "STATUS_ID": "DECLAINED"  }')));
+}
+
+function executeREST($method, $params) {
+
+ 
+   $queryUrl = 'https://comcompany.bitrix24.ua/rest/1/jf43c29vkjox8tqm/'.$method.'.json';
+ 
+   $queryData = http_build_query($params);
+
+ 
+   $curl = curl_init();
+   
+ curl_setopt_array($curl, array(
+        CURLOPT_SSL_VERIFYPEER => 0,
+       
+ CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => 1,
+       
+ CURLOPT_URL => $queryUrl,
+        CURLOPT_POSTFIELDS => $queryData,
+    ));
+
+  
+
+  $result = curl_exec($curl);
+    curl_close($curl);
+
+    return json_decode($result, true);
+
+}
